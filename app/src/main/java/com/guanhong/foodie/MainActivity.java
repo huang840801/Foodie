1102774a9cfaa66e646177886600455a750528ae
huang840801 <david840801@gmail.com>
2018-09-19 12:53:53 +0800

    Initial commit

---
+++ app/src/main/java/com/guanhong/foodie/MainActivity.java
@@ -0,0 +1,13 @@
+package com.guanhong.foodie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
